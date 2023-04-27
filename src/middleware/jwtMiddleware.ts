eadc009e0c4d2a7bb9abd33da98baec5e4a4782d
MuhammadRadifa <m.difa123456@gmail.com>
2023-04-27 16:43:31 +0800

    jwt middleware

---
+++ src/middleware/jwtMiddleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const jwtMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ Message: 'UnAuthorization' });
+  }
+
+  const [authType, token] = authHeader.split(' ');
+
+  if (authType !== 'Bearer' || !token) {
+    return res.status(401).json({ Message: 'UnAuthorization' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err) => {
+    if (err) {
+      return res.status(403).json({ Message: 'UnAuthorization' });
+    }
+
+    next();
+  });
+};
