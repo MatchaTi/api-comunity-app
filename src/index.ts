47397eca13eabcd7d035a3d23844bbc6900b040d
MuhammadRadifa <m.difa123456@gmail.com>
2023-04-30 20:08:56 +0800

    config socket

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import http, { Server } from 'http';
+import { Server as socketServer } from 'socket.io';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { dbConnect } from './databases/connection';
 import indexRoutes from './routes/index';
-import multer from 'multer';
-import { fileFilter, fileStorage } from './config/multer';
 import path from 'path';
 
 dotenv.config();
 
 const app: Express = express();
+const server: Server = http.createServer(app);
+const io: socketServer = new socketServer(server);
 const port = process.env.PORT || 3000;
 
 //connect db
@@ -28,12 +30,20 @@ app.use('/images', express.static(path.join('images')));
 app.set('view engine', 'ejs');
 
 //set route
-app.use('/api/v1', indexRoutes);
+// app.use('/api/v1', indexRoutes);
+
+//io server
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log(msg);
+  });
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome To API Community App');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
