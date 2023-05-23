7e1e3f8c90afc21c0af33d935bc5222215e30c24
MuhammadRadifa <m.difa123456@gmail.com>
2023-05-23 20:14:19 +0800

    update postvalidator

--- src/validator/postValidator.ts
+++ src/validator/postValidator.ts
@@ -1,14 +1,16 @@
 import { body } from 'express-validator';
 
 export const createPostValidator = () => [
-  body('title').isString().isLength({ max: 64 }),
+  body('title').isString().isLength({ max: 80 }),
   body('category').isString(),
-  body('description').isString(),
-  body('code').isString().optional()
+  body('description').isString().isLength({ max: 4000 }).optional(),
+  body('code').isString().optional(),
+  body('link_repo').isString().optional(),
+  body('link_demo').isString().optional()
 ];
 
 export const updatePostValidator = () => [
-  body('title').isString().isLength({ max: 64 }).optional(),
+  body('title').isString().isLength({ max: 80 }).optional(),
   body('category').isString().optional(),
   body('description').isString().optional(),
   body('code').isString().optional()
