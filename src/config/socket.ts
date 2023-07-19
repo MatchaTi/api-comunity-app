534a45cc361e3edae05822407bfe4ace93e1dee3
MuhammadRadifa <m.difa123456@gmail.com>
2023-07-19 16:13:21 +0800

    change function name

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,7 +1,7 @@
 import { Server as socketServer } from 'socket.io';
 import { Server } from 'http';
 
-export const socketInit = (server: Server): socketServer => {
+export const socketInitServer = (server: Server): socketServer => {
   const io: socketServer = new socketServer(server, {
     cors: {
       origin: 'http://localhost:3000',
