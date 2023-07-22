b9e94242e6d714168788619bdedc26124e771efc
MuhammadRadifa <m.difa123456@gmail.com>
2023-07-22 14:06:44 +0800

    add logic to socket controller

--- src/controller/socket/socketController.ts
+++ src/controller/socket/socketController.ts
@@ -1,15 +1,23 @@
 import { Server as socketServer } from 'socket.io';
+import {
+  addUserSocketID,
+  getSocketID,
+  removeUserSocketID
+} from '../../utils/service/socket';
 
 export const socketInitialize = (io: socketServer): void => {
   //io server
   io.on('connection', (socket) => {
-    socket.on('kirim-notifikasi', (pesan) => {
-      console.log(pesan);
-      socket.broadcast.emit('notifikasi-baru', pesan);
+    socket.on('addSocketUserId', (data) =>
+      addUserSocketID(data.username, socket.id)
+    );
+    socket.on('kirim-notifikasi', async (pesan) => {
+      const userSocketID = await getSocketID(pesan.username);
+      socket.to(userSocketID).emit('notifikasi-baru', pesan.data);
     });
-  });
-
-  io.on('disconnect', (reason) => {
-    console.log(reason);
+    io.on('disconnect', () => {
+      console.log('disconnect', socket.id);
+      removeUserSocketID(socket.id);
+    });
   });
 };
