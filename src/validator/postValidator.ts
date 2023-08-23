93cdd1ea7fa294df3464268c8ee3ac680a31f35c
MuhammadRadifa <m.difa123456@gmail.com>
2023-08-23 14:06:23 +0800

    fix  (postValidator) : add length property

--- src/validator/postValidator.ts
+++ src/validator/postValidator.ts
@@ -5,16 +5,16 @@ export const createPostValidator = () => [
     .isString()
     .isLength({ max: 80 })
     .withMessage('Jumlah maximal 80 karakter'),
-  body('category').isString(),
+  body('category').isString().isLength({ max: 30 }),
   body('description')
     .isString()
     .isLength({ max: 4000 })
     .withMessage('Jumlah maximal 4000 karakter')
     .optional(),
   body('syntax').isString().isLength({ max: 4000 }).optional(),
-  body('pathFile').isString().optional(),
-  body('linkSourceCode').isString().optional(),
-  body('linkLiveDemo').isString().optional()
+  body('pathFile').isString().isLength({ max: 128 }).optional(),
+  body('linkSourceCode').isString().isLength({ max: 128 }).optional(),
+  body('linkLiveDemo').isString().isLength({ max: 128 }).optional()
 ];
 
 export const updatePostValidator = () => [
